70f8ab81e15cec6bab8de1581ea181e6a2eafb78
syedmohsin11 <syed.mohsin@ceative.co.uk>
2022-06-09 04:34:52 +0500

    merge-updates

--- swapi/src/App.js
+++ swapi/src/App.js
@@ -6,22 +6,50 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const movieDetails = {
+    title: "New Movie 11",
+  };
+
+  const addMovieHandler = async () => {
+    const response = fetch(
+      "https://react-https-9c624-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movieDetails),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    );
+    const data = await response;
+    console.log("Data from POST :: ", data);
+  };
+
   const loadMovies = useCallback(async () => {
     setLoading(true);
     setError(null);
     try {
-      const url = "https://swapi.dev/api/films";
+      const url =
+        "https://react-https-9c624-default-rtdb.firebaseio.com/movies.json";
       const res = await fetch(url);
       if (!res.ok) {
         throw new Error("Something went Wrong!");
       }
       const data = await res.json();
-      const transformedMovies = data.results.map((item) => {
-        return {
-          title: item.title,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key]?.title,
+        });
+      }
+      // console.log(loadedMovies);
+      // const transformedMovies = data.results.map((item) => {
+      //   return {
+      //     title: item.title,
+      //   };
+      // });
+      setMovies(loadedMovies);
     } catch (err) {
       setError(err.message);
     }
@@ -44,6 +72,9 @@ function App() {
   return (
     <>
       <div className="container p-5">
+        <button onClick={addMovieHandler} className="btn btn-warning me-2">
+          Add Movie
+        </button>
         <button className="btn btn-dark" onClick={loadMovies} type="button">
           {!loading && "Fetch Data"}
           {loading && (
@@ -60,7 +91,7 @@ function App() {
 
         {!loading &&
           movies.length > 0 &&
-          movies.map((item) => <p key={item.title}>{item.title}</p>)}
+          movies.map((item) => <p key={item?.id}>{item?.title}</p>)}
         {!loading && movies.length === 0 && !error && <p>No Data Found.</p>}
         {!loading && error && <p>{error}</p>}
       </div>
